aab1fa1f5e15878043b61e29471e906d873493cd
HELLSHUBH <shubhgaikwad654@gmail.com>
2026-05-03 19:51:18 +0000

    m

--- backend/database.py
+++ backend/database.py
@@ -7,7 +7,7 @@ load_dotenv()
 DB_CONFIG = {
     'host': os.getenv("DB_HOST"),
     'user': os.getenv("DB_USER"),
-    'port': int(os.getenv("DB_PORT", 3306)),
+    'port': int(os.getenv("DB_PORT", "3306")),
     'password': os.getenv("DB_PASSWORD"),
     'database': os.getenv("DB_NAME")
 }
