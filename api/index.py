bfe72077e4a3961aadc519ca675d1f29ead7d6da
HELLSHUBH <shubhgaikwad654@gmail.com>
2026-05-09 16:13:47 +0000

    Backend in DIR changed to backend

--- api/index.py
+++ api/index.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 # inside your Backend folder.
 # ---------------------------------------------------
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-BACKEND_DIR = os.path.join(ROOT_DIR, "Backend")
+BACKEND_DIR = os.path.join(ROOT_DIR, "backend")
 
 if BACKEND_DIR not in sys.path:
     sys.path.insert(0, BACKEND_DIR)
